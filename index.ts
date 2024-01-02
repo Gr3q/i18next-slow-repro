bab99c825e8d22dd91b8d4d6c991487ae9f4af97
Attila Greguss <attila.greguss@netduma.com>
2024-01-02 10:23:09 +0000

    Update repro with latest fix

--- index.ts
+++ index.ts
@@ -4,10 +4,25 @@ function Workaround(): Extract<ParseKeys<["misc"]>, "misc:24GHz"> {
     return "misc:24GHz";
 }
 
-function ReallySlow(): ParseKeys<["misc"]> {
+function NotSlowAnymore(): ParseKeys<["misc"]> {
     return "misc:24GHz";
 }
 
-t(ReallySlow())
+function NotSlowOrdinal(): ParseKeys<["misc"], { ordinal: true }> {
+    return "misc:24GHz";
+}
+
+function NotSlowCount(): ParseKeys<["misc"], { count: 2 }> {
+    return "misc:24GHz";
+}
+
+t(NotSlowOrdinal(), { ordinal: true })
+t(NotSlowCount(), { ordinal: true })
+t(NotSlowOrdinal(), { count: 3 })
+t(NotSlowCount(), { count: 3 })
+// With 23.7.13
+t(NotSlowAnymore(), { count: 1 })
+t(NotSlowAnymore(), { ordinal: true })
+t(NotSlowAnymore())
 t(Workaround())
 
